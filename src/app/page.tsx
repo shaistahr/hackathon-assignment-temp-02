48e266cffb6287735b41e6ce374c61fbeb32f621
shaistahr <shazzkhan754@gmail.com>
2024-12-09 10:51:37 -0800

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,23 @@
+
+import FeaturesSection from "./components/Features-section";
+import Footer from "./components/footer";
+import HeroBanner from "./components/Hero-banner";
+import HeroSection from "./components/Hero-section";
+import { Navbar } from "./components/Navbar";
+import NewsletterSection from "./components/News";
+
+
+
+export default function Home() {
+  return (
+    <>
+    <Navbar/>
+    <HeroBanner />
+    <FeaturesSection/>
+    <HeroSection/>
+    <NewsletterSection/>
+    <Footer/>
+    </>
+  )
+}
+
