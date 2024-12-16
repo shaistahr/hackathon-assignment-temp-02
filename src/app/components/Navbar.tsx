5f4874bcb72ef68050531fcb61bc8eba50f4c712
shaistahr <shazzkhan754@gmail.com>
2024-12-16 11:14:20 -0800

    Bug Fixed

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -112,6 +112,7 @@ export function Navbar() {
                       className="text-gray-500 hover:text-gray-900 px-3 py-2 rounded-md text-sm font-medium transition-colors duration-200 ease-in-out"
                     >
                       {item}
+                      {/* Comment */}
                     </Link>
                   ))}
                   <Button variant="ghost" size="sm" className="justify-start text-gray-400 hover:text-gray-500">
