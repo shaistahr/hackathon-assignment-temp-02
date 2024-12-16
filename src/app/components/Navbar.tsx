355def84a3b4de4d37b963e601a5a3a8d6d139ee
shaistahr <shazzkhan754@gmail.com>
2024-12-16 11:05:45 -0800

    Add About Page

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -108,7 +108,7 @@ export function Navbar() {
                   {navItems.map((item) => (
                     <Link
                       key={item}
-                      href={`/${item}`}
+                      href={`/${item.toLowerCase()}`}
                       className="text-gray-500 hover:text-gray-900 px-3 py-2 rounded-md text-sm font-medium transition-colors duration-200 ease-in-out"
                     >
                       {item}
