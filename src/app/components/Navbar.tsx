df67d63e545e43d53277ef5e2934b3e9efd4b894
shaistahr <shazzkhan754@gmail.com>
2024-12-16 11:08:30 -0800

    Update

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -133,7 +133,7 @@ export function Navbar() {
           {navItems.map((item) => (
             <Link
               key={item}
-              href={`/${item}`}
+              href={`/${item.tolowerCase()}`}
               className="text-gray-500 hover:text-gray-900 px-3 py-2 rounded-md text-sm font-medium transition-colors duration-200 ease-in-out"
             >
               {item}
