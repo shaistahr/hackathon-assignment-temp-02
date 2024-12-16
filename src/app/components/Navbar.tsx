7e32ed1502aa52934030e3e3563d01d57c619523
shaistahr <shazzkhan754@gmail.com>
2024-12-16 11:03:02 -0800

    Update

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -108,7 +108,7 @@ export function Navbar() {
                   {navItems.map((item) => (
                     <Link
                       key={item}
-                      href={`${item}`}
+                      href={`/${item}`}
                       className="text-gray-500 hover:text-gray-900 px-3 py-2 rounded-md text-sm font-medium transition-colors duration-200 ease-in-out"
                     >
                       {item}
@@ -133,7 +133,7 @@ export function Navbar() {
           {navItems.map((item) => (
             <Link
               key={item}
-              href={`${item}`}
+              href={`/${item}`}
               className="text-gray-500 hover:text-gray-900 px-3 py-2 rounded-md text-sm font-medium transition-colors duration-200 ease-in-out"
             >
               {item}
